a76249287b0f1ba9584b74ae66cbfc1866b67be7
Hugo Ignacio <hugo.ignacio1998@gmail.com>
2023-01-27 04:07:17 -0300

    fix: fix names in circuitSchema

--- src/schemas/circuit-schema.ts
+++ src/schemas/circuit-schema.ts
@@ -1,8 +1,8 @@
 import joi from "joi";
 
 export const circuitSchema = joi.object({
-  racer: joi.number().required(),
-  car: joi.number().required(),
-  track: joi.number().required(),
-  time: joi.number().required(),
+  racer_id: joi.number().required(),
+  car_id: joi.number().required(),
+  track_id: joi.number().required(),
+  time_lap: joi.number().required(),
 });
